078a507dfc435d34ef00b188d48a2c0e78896f22
Ernesferre <70835371+Ernesferre@users.noreply.github.com>
2021-04-23 16:21:38 -0300

    mejorando estilos

--- src/components/Equipo.js
+++ src/components/Equipo.js
@@ -47,7 +47,37 @@ const Equipo = () => {
 
             <h1 className="text-white justify-content-center justify-content-center">Equipo</h1>
 
-            <h3 className="text-white text-center">En esta seccion debo poder visualizar los 6 Heroes seleccionados</h3>
+            <h2 className="text-white text-center">Este seria el HOME de la app. Esta seccion debo poder visualizar los 6 Heroes seleccionados.</h2>
+            <h3 className="text-white text-center"> Cada item del Listado debe contener el NOMBRE del Heroe, su IMAGEN y sus POWERSTATS</h3>
+            <hr/>
+            <h3 className="text-white text-center list-group list-styled" > POWERSTATS:</h3>
+            <hr/>
+            <ul className="text-white  text-center type-circle mb-5 bg-primary">
+
+              <li> Intelligence </li>
+              <li> Strength </li>
+              <li> Speed </li>
+              <li> Durability </li>
+              <li> Power </li>
+              <li> Combat </li>
+
+            
+            </ul>
+
+            <h3 className="text-white text-left mb-5 text-warning"> En esta pantalla se debera mostrar ademas:</h3>
+            <ol className="text-white type-circle">
+
+              <li> Acumulativo de Powerstats, agrupados por cada uno, es decir... suma total de Inteligencia, Strenght, etc </li>
+              <li> El Powerstats que mas acumulativo tenga debera apareecer arriba para categorizar el tipo de Equipo </li>
+              <li> Pesos y Alturas Promedios del Equipo</li>
+              <li> 3 miembros del equipo debera tener Orientacion "Buena" y los otros 3 "Mala". Esto debe validarse al intentar agregar un nuevo heroe </li>
+              <li> Se debera Poder Eliminar un miembro del equipo, lo que generará un nuevo promedio de pesos y acumulativos de Powerstats  </li>
+              
+
+            
+            </ol>
+
+            
                 
         </div>
 
